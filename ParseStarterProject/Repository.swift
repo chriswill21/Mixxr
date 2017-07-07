2c3d789481ced1c208f343380814616e39ac8536
Yaateh <yaatehr@mit.edu>
2017-07-07 00:51:54 -0400

    minor fix

--- ParseStarterProject/Repository.swift
+++ ParseStarterProject/Repository.swift
@@ -5,20 +5,20 @@
 //  Created by Yaateh on 7/3/17.
 //  Copyright © 2017 Parse. All rights reserved.
 //
-//
-//import Foundation
-//import Parse
-//
-//
-//protocol Repository  {
-//    associatedtype T
-//    
-//    //query for the event values based on user location or view location
-//    //store events locally
-//    //render the 
-//    
-//    func getAll() -> [T]
-//    func getById(id: Int) -> T
-//    func getByLocation(user: )
-//    
-//}
+
+import Foundation
+import Parse
+
+
+protocol Repository  {
+   associatedtype T
+   
+   //query for the event values based on user location or view location
+   //store events locally
+   //render the 
+   
+   func getAll() -> [T]
+   func getById(id: Int) -> T
+   func getByLocation(user: )
+   
+}
