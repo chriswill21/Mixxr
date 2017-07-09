2d158b268e3d722c9a3a7f8a86ba563f477c5b49
Yaateh <yaatehr@mit.edu>
2017-07-09 13:19:18 -0400

    Repository pattern and minor edit. This .xcodeproj stuff is starting to bug me...

--- ParseStarterProject/Repository.swift
+++ ParseStarterProject/Repository.swift
@@ -8,17 +8,79 @@
 
 import Foundation
 import Parse
+import CoreLocation
 
 
-//protocol Repository  {
-//   associatedtype T
-//   
-//   //query for the event values based on user location or view location
-//   //store events locally
-//   //render the 
-//   
-//   func getAll() -> [T]
-//   func getById(id: Int) -> T
-//   func getByLocation(user: )
-//   
-//}
+
+// we could also use open class Repository<Entity: PFObject>
+// I think protocols are slightly easier to replicate
+
+protocol Repository  {
+    associatedtype Entity
+    
+   
+   //query for the event values based on user location or view location
+   //store events locally
+   //render the
+    init()
+    //getters
+    func getAll(cache: Bool? ) -> [Entity]
+    func getById(id: Int, cache: Bool?) -> Entity
+    func getByLocation(location: PFGeoPoint, cache: Bool?) -> [Entity]
+    func getByLatLon(lat: Double, lon: Double, cache: Bool?) -> [Entity]
+    func getByCLLocation()//stub
+
+    //Create Update Delete
+    func updateById(id: Int) -> Bool
+    
+    func create(object: Entity) -> Bool
+    
+    func deleteById(id: Int)
+}
+
+class EventRepository: Repository   {
+    typealias Entity = PFObject
+
+    private var _user:PFUser
+    private var _locManager:CLLocationManager
+//    private var _query: PFQuery
+    
+    required init(){
+        
+        self._user = PFUser.current()!
+        self._locManager = CLLocationManager()
+//        self._query = PFQuery(className: "Event")
+        
+    }
+    //getters
+    @discardableResult func getAll(cache: Bool? ) -> [Entity] {
+        var query = PFQuery(className: "Event")
+    }
+    func getById(id: Int, cache: Bool?) -> Entity {
+        
+    }
+    @discardableResult func getByLocation(location: PFGeoPoint, cache: Bool?) -> [Entity] {
+        
+    }
+    @discardableResult func getByLatLon(lat: Double, lon: Double, cache: Bool?) -> [Entity] {
+        
+    }
+    func getByCLLocation(){
+        
+    }//stub
+    
+    //Create Update Delete
+    func updateById(id: Int) -> Bool {
+        
+    }
+    
+    func create(object: Entity) -> Bool {
+        
+    }
+    
+    func deleteById(id: Int) -> Bool {
+        
+    }
+}
+
+
