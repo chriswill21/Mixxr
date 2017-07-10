32780ed6e36f2e4fd83cbf443829f61bbc0efdb1
Yaateh <yaatehr@mit.edu>
2017-07-10 08:02:47 -0400

    readme update and minor changes

--- ParseStarterProject/Repository.swift
+++ ParseStarterProject/Repository.swift
@@ -1,86 +1,126 @@
-////
-////  Repository.swift
-////  ParseStarterProject-Swift
-////
-////  Created by Yaateh on 7/3/17.
-////  Copyright © 2017 Parse. All rights reserved.
-////
 //
-//import Foundation
-//import Parse
-//import CoreLocation
+//  Repository.swift
+//  ParseStarterProject-Swift
 //
+//  Created by Yaateh on 7/3/17.
+//  Copyright © 2017 Parse. All rights reserved.
 //
-//
-//// we could also use open class Repository<Entity: PFObject>
-//// I think protocols are slightly easier to replicate
-//
-//protocol Repository  {
-//    associatedtype Entity
-//    
-//   
-//   //query for the event values based on user location or view location
-//   //store events locally
-//   //render the
-//    init()
-//    //getters
-//    func getAll(cache: Bool? ) -> [Entity]
-//    func getById(id: Int, cache: Bool?) -> Entity
-//    func getByLocation(location: PFGeoPoint, cache: Bool?) -> [Entity]
-//    func getByLatLon(lat: Double, lon: Double, cache: Bool?) -> [Entity]
-//    func getByCLLocation()//stub
-//
-//    //Create Update Delete
-//    func updateById(id: Int) -> Bool
-//    
-//    func create(object: Entity) -> Bool
-//    
-//    func deleteById(id: Int)
-//}
-//
-//class EventRepository: Repository   {
-//    typealias Entity = PFObject
-//
-//    private var _user:PFUser
-//    private var _locManager:CLLocationManager
-////    private var _query: PFQuery
-//    
-//    required init(){
-//        
-//        self._user = PFUser.current()!
-//        self._locManager = CLLocationManager()
-////        self._query = PFQuery(className: "Event")
-//        
-//    }
-//    //getters
-//    @discardableResult func getAll(cache: Bool? ) -> [Entity] {
-//        var query = PFQuery(className: "Event")
-//    }
-//    func getById(id: Int, cache: Bool?) -> Entity {
-//        
-//    }
-//    @discardableResult func getByLocation(location: PFGeoPoint, cache: Bool?) -> [Entity] {
-//        
-//    }
-//    @discardableResult func getByLatLon(lat: Double, lon: Double, cache: Bool?) -> [Entity] {
-//        
-//    }
-//    func getByCLLocation(){
-//        
-//    }//stub
-//    
-//    //Create Update Delete
-//    func updateById(id: Int) -> Bool {
-//        
-//    }
-//    
-//    func create(object: Entity) -> Bool {
-//        
-//    }
-//    
-//    func deleteById(id: Int) -> Bool {
-//        
-//    }
-//}
-//
-//
+
+import Foundation
+import Parse
+import CoreLocation
+
+
+
+// we could also use open class Repository<Entity: PFObject>
+// I think protocols are slightly easier to replicate
+
+protocol Repository  {
+    associatedtype Entity
+    
+   
+   //query for the event values based on user location or view location
+   //store events locally
+   //render the
+    init()
+    //getters
+    func getAll(cache: Bool? ) -> [Entity]
+    func getById(id: String, cache: Bool?) -> Entity
+    func getByLocation(location: PFGeoPoint, cache: Bool?) -> [Entity]
+    func getByLatLon(lat: Double, lon: Double, cache: Bool?) -> [Entity]
+    func getByCLLocation()//stub
+
+    //Create Update Delete
+    func updateById(id: String) -> Bool
+    
+    func create(object: Entity) -> Bool
+    
+    func deleteById(id: String)
+}
+
+class EventRepository: Repository   {
+    typealias Entity = PFObject
+
+    private var _user:PFUser
+    private var _locManager:CLLocationManager
+    private var eventsInitialized: Bool;
+//    private var _query: PFQuery
+    
+    required init(){
+        self.eventsInitialized = false
+        self._user = PFUser.current()!
+        self._locManager = CLLocationManager()// we might want to consider injecting the repo class into a bigger one and letting that one manage, update adn refresh user location
+//        self._query = PFQuery(className: "Event")
+        
+    }
+    //getters
+    @discardableResult func getAll(cache: Bool? ) -> [Entity]? {
+        let query = PFQuery(className: "Event")
+        var results = [Entity]()
+        query.findObjectsInBackground(block: { (objects, error) in
+            if error == nil {
+                results = objects // todo: Implemetn comparator in Parse to sort by location
+            }
+            else {
+                print("Error: \(error!)")
+            }
+        })
+        if (!results.isEmpty){
+            return results
+        }
+    }
+    
+    
+    @discardableResult func getById(objectid id: String, cache: Bool?) -> Entity? {
+        let query = PFQuery(className: "Event")
+        var result:Entity?
+        query.getObjectInBackground( withId: id, block: { (object, error) in
+            if error == nil {
+                result = object // you have to pass this to the exterior or you get a Unexpected non-void return value in void function
+            }
+            else {
+                print("Error: \(error!)")
+            }
+            })
+        return result
+    }
+    
+    
+    @discardableResult func getByLocation(location: PFGeoPoint, cache: Bool?) -> [Entity]? {
+        let query = PFQuery(className: "Event")
+        var results = [Entity]()
+        query.findObjectsInBackground(block: { (objects, error) in
+            if error == nil {
+                results = objects // todo: Implemetn comparator in Parse to sort by location
+            }
+            else {
+                print("Error: \(error!)")
+            }
+        })
+        if (!results.isEmpty){
+            return results
+        }
+    }
+    
+    @discardableResult func getByLatLon(lat: Double, lon: Double, cache: Bool?) -> [Entity] {
+        
+    }
+    func getByCLLocation(){
+        
+    }//stub
+    
+    //Create Update Delete
+    func updateById(id: Int) -> Bool {
+        
+    }
+    
+    func create(object: Entity) -> Bool {
+        
+    }
+    
+    func deleteById(id: Int) -> Bool {
+        
+    }
+}
+
+
--- README.md
+++ README.md
@@ -1,19 +1,27 @@
 # Mixxr
 
-##Cocoa Pod installation
+## Cocoa Pod installation
 if you are getting build errors (missing .lock files)
 run the following commands in your root:
 
 > sudo gem install cocoapods
+
 > pod install
 
 make sure your podfile has all the following lines
 
 >pod 'Firebase/Core', '~> 4.0.0'
+
 >pod 'Firebase/Auth', '~> 4.0.0'
+
 >pod 'Firebase/Core', '~> 4.0.0'
+
 >pod 'SCLAlertView'
 
 and then run
 
->pod update
+> pod update
+
+> open ParseStarterProject-Swift.xcworkspace
+
+and make sure that the GoogleService-Info.plist is in ur xcode project in the a ParseStarterProject
